385daad0217e080e28ba9e14745683938594e92f
djdjdddd <djdj2297@gmail.com>
2024-01-06 22:47:30 +0900

    Feat: 스프링 MVC 1편 프로젝트 재생성

---
+++ spring-mvc/spring-mvc-1/src/test/java/com/springmvc1/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
