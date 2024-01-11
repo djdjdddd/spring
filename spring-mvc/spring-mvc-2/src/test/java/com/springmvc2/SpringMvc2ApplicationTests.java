77e4bf077d9eefaa4800df9111c244336340ea56
djdjdddd <djdj2297@gmail.com>
2024-01-11 22:52:53 +0900

    Feat: 스프링 MVC 2편 프로젝트 생성

---
+++ spring-mvc/spring-mvc-2/src/test/java/com/springmvc2/SpringMvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
