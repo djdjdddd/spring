16ab6a330e0e57d5348c392969b06e910bfb8c9f
djdjdddd <djdj2297@gmail.com>
2024-02-24 23:39:32 +0900

    Feat: 섹션 7. 서블릿 필터 - 인증 체크

--- spring-mvc/spring-mvc-2/src/main/java/com/springmvc2/login/web/filter/LoginCheckFilter.java
+++ spring-mvc/spring-mvc-2/src/main/java/com/springmvc2/login/web/filter/LoginCheckFilter.java
@@ -1,5 +1,6 @@
 package com.springmvc2.login.web.filter;
 
+import com.springmvc2.login.web.SessionConst;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,40 +9,50 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.PatternMatchUtils;
 
 import java.io.IOException;
-import java.util.function.Predicate;
 
+/**
+ * @author : yong
+ * @fileName : LoginCheckFilter
+ * @date : 2024-02-24
+ * @description :
+ */
 @Slf4j
 public class LoginCheckFilter implements Filter {
 
-    private static final String[] whitelist = {"/", "/members/add", "/login", "/logout", "/css/*"}; // 로그인 안했다고 해서 CSS 파일 요청까지 막아버리면 안되겠죠? ㅎㅎ..
-
-    // cf. init(), destroy()는 인터페이스의 디폴트 메서드이기 때문에 굳이 구현할 필요 없다. (그리고 Filter 클래스 생성할 때마다 매번 그렇게 초기화/삭제 할 필요 없기도 하고...)
+    private static final String[] whitelist = {"/", "members/add", "login", "logout", "/css/*"};
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 우선 request, response 둘 다 타입 캐스팅 (HTTP 요청 전문 타입임. 일반 ServletReq, Res는 HTTP 요청 외에 다른 것도 받을 수 있게 설계돼있다고 함)
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpRequest.getRequestURI();
 
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
 
         try {
             log.info("인증 체크 필터 시작 {}", requestURI);
-
             if(isLoginCheckPath(requestURI)){
                 log.info("인증 체크 로직 실행 {}", requestURI);
-                HttpSession session = httpRequest.getSession(false);
+                HttpSession session = httpRequest.getSession(false);// 세션을 얻되 현재 세션이 존재하지 않으면 새로 생성하지 않고 null을 리턴한다. (구현체 메서드를 까보면 알 수 있다)
                 if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null){
                     log.info("미인증 사용자 요청 {}", requestURI);
+                    // 로그인으로 redirect
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI); // 로그인 후 클라이언트가 원래 요청했던 해당 URI로 다시 리다이렉트 시켜주기 위함. (LoginController의 loginV4 메서드 참조)
+                    return; // 여기가 중요! 미인증 사용자는 다음으로 진행하지 않고 로직 수행을 끝낸다!
                 }
             }
-        }catch ()
+            filterChain.doFilter(servletRequest, httpResponse);
+        }catch (Exception e){
+            throw e; // 예외 로깅 가능하지만, throw 하여 WAS(톰캣)까지 예외를 보내주어야 함.
+        }finally {
+            log.info("인증 체크 필터 종료 {}", requestURI);
+        }
     }
 
     /**
      * 화이트 리스트의 경우 인증 체크 X
      */
     private boolean isLoginCheckPath(String requestURI){
-        return !PatternMatchUtils.simpleMatch(whitelist, requestURI); // PatternMatchUtils : 스프링이 제공하는 Util. 둘 간의 패턴이 매칭되는지 체크해준다. 이걸 이용하여 편하게 코드를 짜도록 하자
+        // requestURI 가 화이트 리스트와 매치하는지 체크한다.
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
     }
 }
