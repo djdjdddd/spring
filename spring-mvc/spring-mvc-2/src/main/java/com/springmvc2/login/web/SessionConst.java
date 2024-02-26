9b1384c6a3c5140039c9fbff0cc691311e040e94
djdjdddd <djdj2297@gmail.com>
2024-02-26 22:50:52 +0900

    Feat: SessionConst

---
+++ spring-mvc/spring-mvc-2/src/main/java/com/springmvc2/login/web/SessionConst.java
@@ -0,0 +1,11 @@
+package com.springmvc2.login.web;
+
+/**
+ * @author : yong
+ * @fileName : SessionConst
+ * @date : 2024-02-24
+ * @description :
+ */
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
